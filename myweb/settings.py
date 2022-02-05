c946009f4ba80a7e4ce552ca26bc0c3fa13f16ea
mdasifurrahman <asifurr959@gmail.com>
2022-02-05 16:44:39 +0600

    V0.3

--- myweb/settings.py
+++ myweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-66ud!&n&ptz_e)y_9^+i_%$b-lpsu24l!p961nr4=+d0k4vy!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ayonswebportal.herokuapp.com','127.0.0.1']
 
@@ -123,5 +123,8 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- webpage/views.py
+++ webpage/views.py
@@ -3,6 +3,9 @@ import fuzzywuzzy
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 # Create your views here.
+from django.core.files.storage import FileSystemStorage
+import PyPDF2
+
 
 def home(request):
     return render(request,'webpage/index.html')
@@ -22,3 +25,48 @@ def similarity(request):
 
         }
         return render(request, 'webpage/similar.html', context)
+
+
+def upload(request):
+    if request.method == "GET":
+        return render(request, 'webpage/upload.html')
+
+    if request.method == "POST":
+        upload_file1 = request.FILES['document1']
+        upload_file2 = request.FILES['document2']
+        # print(upload_file.name)
+        # print(upload_file.size)
+
+        #document1
+        fs = FileSystemStorage()
+        filename1 = fs.save(upload_file1.name, upload_file1)
+        uploaded_file_url1 = fs.url(filename1)
+
+#document2
+        filename2 = fs.save(upload_file2.name, upload_file2)
+        uploaded_file_url2 = fs.url(filename2)
+
+        pdffileobj1 = open(r"media\{}".format(filename1), 'rb')
+        pdfreader1 = PyPDF2.PdfFileReader(pdffileobj1)
+        pageobj1 = pdfreader1.getPage(0)
+        arr1 = []
+        arr1.append(pageobj1.extractText())
+
+        pdffileobj2 = open(r"media\{}".format(filename2), 'rb')
+        pdfreader2 = PyPDF2.PdfFileReader(pdffileobj2)
+        pageobj2 = pdfreader2.getPage(0)
+        arr2 = []
+        arr2.append(pageobj2.extractText())
+        ratio = fuzz.ratio(arr1,arr2)
+
+
+        # print(pageobj1.extractText())
+
+        context = {
+                # 'uploaded_file_url1': uploaded_file_url1
+            'answer' : ratio
+
+        }
+        # print(upload_file2.name)
+        # print(upload_file2.size)
+        return render(request,'webpage/upload.html', context)
