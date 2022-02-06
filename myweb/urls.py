406d5b3c8c88c9cd71231447f259891b13c9bee8
mdasifurrahman <asifurr959@gmail.com>
2022-02-06 15:09:51 +0600

    V0.4

--- myweb/urls.py
+++ myweb/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('upload/', webviwes.upload),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# class Document(models.Model):
+#     document1 = models.FileField(upload_to='documents/')
+#     document2 = models.FileField(upload_to='documents/')
+
